954333f9e524c97a4794ec8ce5165b5571db098a
yashraj <yashrajprajapati4002@gmail.com>
2025-12-08 16:16:17 +0530

    Add first file

---
+++ Basic.c
@@ -0,0 +1,89 @@
+ //                                     {   Chapter 1   }
+  
+ //                                    ~~ Start C codes ~~
+
+
+
+ // Basic Structur of code
+ #include<stdio.h>
+ // Function Decliration 
+ int main()
+ {
+    printf("Hello World");
+    // all code runimg part (main part of code)
+    return 0;
+ }
+
+
+
+
+ // [Que 1] Add two or more values ?
+ /*
+ #include<stdio.h>
+ int main()
+ {
+    int a, b;
+    printf("Enter a : ");
+    scanf("%d",&a);
+    printf("Enter b : ");
+    scanf("%d",&b);
+    printf("sum is : %d", a+b);
+    return 0;
+ }*/
+
+
+
+ // [Que 2] Write a program to calculate area of a square ?
+ /*#include<stdio.h>
+ int main()
+ {
+    int side;
+    printf("Enter side : ");
+    scanf("%d", &side );
+    printf("Square area is : %d", side*side);
+    return 0;
+ }*/
+
+
+
+// [Que 3] Write a p calculate area of circle ?
+/*#include<stdio.h>
+int main()
+{
+    float radius;
+    printf("Circle radius : ");
+    scanf("%f",&radius);
+    printf("Area is : %f", 3.14 * radius * radius);
+    return 0;
+}*/
+
+
+
+// [Que 4] Write calculate area and parameter of rectangle ?
+/*#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("Length of Rect.. :");
+    scanf("%d", &a);
+    printf("Breadth of Rect..: ");
+    scanf("%d", &b);
+    printf("Area of rectangle is : %d\n",a*b);
+    printf("Parameter of rectangle is : %d", 2*(a+b));
+    return 0;
+}*/
+
+
+
+// [Que 5] Write to take a no.(n) from user & outputs its cube(n*n*n).
+/*#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a no. : ");
+    scanf("%d",&n);
+    printf("Cube of (n) is :%d",n*n*n);
+    return 0;
+}*/
+
+
